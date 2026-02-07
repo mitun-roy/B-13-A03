e212970e4fbcc315e0ce84d13f4ce3333365a427
mitun-roy <mitunroy.yh@gmail.com>
2026-02-07 19:10:15 +0600

    problem1 resolve

--- problem1.js
+++ problem1.js
@@ -3,11 +3,11 @@ function newPrice(currentPrice , discount ) {
     if ( typeof currentPrice !=='number' || 100<discount>0){
         return 'invalide';
     }
-   const result = currentPrice * (discount/100);
-   
+     const discountPrice = (currentPrice * discount)/100;
+        const result = currentPrice - discountPrice;
     
   return result.toFixed(3);
           
 }
-const result = newPrice(1500,200);
+const result = newPrice(2000,17.17);
 console.log(result);
